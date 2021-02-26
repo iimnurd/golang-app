94d1af23031a2f0a6c5b691d9f8e43581bc3792e
“iimnurd” <iim@alterra.id>
2021-02-26 19:24:31 +0700

    change main.go

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module gorest
 
 go 1.12
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/uber/jaeger-client-go v2.25.0+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import(
-	"net/http"
+    "net/http"
     "encoding/json"
     "math/rand"
     "time"
@@ -12,6 +12,14 @@ import(
     "github.com/joho/godotenv"
     "os"
     "strconv"
+    "context"
+
+    "github.com/opentracing/opentracing-go"
+    "github.com/opentracing/opentracing-go/ext"
+    "github.com/uber/jaeger-client-go"
+    jaegercfg "github.com/uber/jaeger-client-go/config"
+    jaegerlog "github.com/uber/jaeger-client-go/log"
+    "github.com/uber/jaeger-lib/metrics"
  
 )
 var key string = ""
@@ -21,7 +29,7 @@ type EndTime float64
 
 //User defines model for storing account details in database
 type Request struct {
-	Id string `json:"id"`
+    Id string `json:"id"`
     Request []string `json:"request"`
    // CreatedAt time.Time
 }
@@ -58,13 +66,103 @@ func init() {
 
 func main(){
    
-	mux := http.NewServeMux()
+    mux := http.NewServeMux()
+
+    log.Println("starting...")
+
+    cfg := jaegercfg.Configuration{
+        ServiceName: "app1",
+        Sampler: &jaegercfg.SamplerConfig{
+            Type:  jaeger.SamplerTypeConst,
+            Param: 1, // trace every call
+        },
+        Reporter: &jaegercfg.ReporterConfig{
+            LogSpans: false,
+        },
+    }
+
+    jLogger := jaegerlog.StdLogger
+    jMetricsFactory := metrics.NullFactory
+
+    tracer, closer, err := cfg.NewTracer(
+        jaegercfg.Logger(jLogger),
+        jaegercfg.Metrics(jMetricsFactory),
+    )
+
+    if err != nil {
+        log.Fatalf("could not initialize jaeger tracer: %s", err.Error())
+    }
+    defer closer.Close()
+
+    opentracing.SetGlobalTracer(tracer)
+
+    f1 := traceF1(tracer)
+
+
 
     mux.HandleFunc("/webhook", echoHandler)
     mux.HandleFunc("/health", healthcheck)
-
-	http.ListenAndServe(":8000", mux)
+    mux.HandleFunc("/jaeger", f1)
+    http.ListenAndServe(":8000", mux)
 }
+
+func traceF1(tracer opentracing.Tracer) func(w http.ResponseWriter, r *http.Request) {
+    return func(w http.ResponseWriter, r *http.Request) {
+
+        spanCtx, _ := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
+        span := tracer.StartSpan("start", ext.RPCServerOption(spanCtx))
+        defer span.Finish()
+
+        span.Context().ForeachBaggageItem(func(k, v string) bool {
+            fmt.Println(span, "baggage:", k, v)
+            span.LogKV(k, v)
+            return true
+        })
+
+        ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+        defer cancel()
+
+        ctx = opentracing.ContextWithSpan(ctx, span)
+
+        s, return1 := f1(ctx, tracer)
+
+        w.WriteHeader(http.StatusOK)
+        fmt.Fprintf(w, s+return1)
+        return
+    }
+}
+
+func f1(ctx context.Context, tracer opentracing.Tracer) (string, string) {
+
+    // span := root.Tracer().StartSpan("f1", opentracing.ChildOf(root.Context()))
+    span, ctx := opentracing.StartSpanFromContext(ctx, "f1")
+    defer span.Finish()
+
+    sleept := time.Duration(rand.Intn(1120)) * time.Millisecond
+    time.Sleep(sleept)
+
+    return1 := f2(ctx)
+    
+
+    s := "<html><body>f1 done:"
+
+    return s, return1
+
+}
+
+func f2(ctx context.Context) string {
+    span, ctx := opentracing.StartSpanFromContext(ctx, "f2")
+    defer span.Finish()
+
+    sleept := time.Duration(rand.Intn(1920)) * time.Millisecond
+    span.LogKV("sleep", sleept)
+    time.Sleep(sleept)
+
+    return "f2 done:"
+}
+
+
+
 
 func (s Resp_time) MarshalJSON() ([]byte, error) {
     data := map[string]interface{}{
@@ -92,7 +190,7 @@ func action(data Request, StartTime time.Time) (jsonInBytes []byte){
         }else {
         key = os.Getenv("APP_NAME")
         }
-	jsonInBytes, _ = json.Marshal(datas)
+    jsonInBytes, _ = json.Marshal(datas)
     
     
 
@@ -182,27 +280,27 @@ func getResponse(body []byte) (*Response, error) {
 func echoHandler(w http.ResponseWriter, r *http.Request){
     start := time.Now()
 
-	request := Request{} //initialize empty user
+    request := Request{} //initialize empty user
     
-	//Parse json request body and use it to set fields on user
-	//Note that user is passed as a pointer variable so that it's fields can be modified
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil{
-		panic(err)
+    //Parse json request body and use it to set fields on user
+    //Note that user is passed as a pointer variable so that it's fields can be modified
+    err := json.NewDecoder(r.Body).Decode(&request)
+    if err != nil{
+        panic(err)
     }
     
     if (len(request.Request)<= 1){
         b := action(request, start)
         
-	w.Header().Set("Content-Type","application/json")
-	
+    w.Header().Set("Content-Type","application/json")
+    
     w.Write(b)
     
 
     }else {
         b := forward(request, start, r)
         w.Header().Set("Content-Type","application/json")
-	
+    
         w.Write(b)
     
 
@@ -213,5 +311,10 @@ func echoHandler(w http.ResponseWriter, r *http.Request){
         w.Write([]byte("OK"))
 
 
-	
+    
 }
+// A function to be wrapped
+func slowFunc(s string, c chan string) {
+    time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
+    c <- "received " + s
+   }
